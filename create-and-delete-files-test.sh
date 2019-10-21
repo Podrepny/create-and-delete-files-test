f4a2154f3016067c97185c6f2dff5df049a6b55d
Podrepny <31345196+Podrepny@users.noreply.github.com>
2019-10-21 10:50:19 +0300

    Add test and verbose mode
    Add test and verbose mode

--- create-and-delete-files-test.sh
+++ create-and-delete-files-test.sh
@@ -8,8 +8,9 @@ DISK_PATH="/dev/sdb1"
 MOUNT_PATH="/test"
 DISK_SIZE=$(fdisk -l ${DISK_PATH} | head -n 1 | tr -s ' ' | cut -d ' ' -f 5)
 INODES_NUM=10000000
-NUM_FILES=9000000
+NUM_FILES=1000
 EXITCODE=0
+VERBOSE=true
 # DISCRETENESS=1000
 
 VARSAFTER=`compgen -v`
@@ -47,86 +48,112 @@ function FN_DECORATE {
 }
 
 function FN_CREATE_FILES {
-    echo -e "\e[32m\tCreating\e[36m ${NUM_FILES} \e[32mfiles in\e[36m ${MOUNT_PATH}\e[0m"
-    for (( i=1; i <= ${NUM_FILES}; i++ )); do
-        touch ${MOUNT_PATH}/$i.file
-        if [[ $((i % 1000)) = 0 ]]; then
-            progress=$(echo "scale=3; ${i}/${NUM_FILES}*100" | bc)
-            echo -ne "\e[?25l\e[s\e[2K${progress}% complete (files $i of ${NUM_FILES})\e[u"
-            # echo -ne "\e[?25l\e[s\e[2K$i of ${NUM_FILES}\e[u"
-        fi
-    done
-    echo -e "\e[36m\t${NUM_FILES} \e[32mfiles have been created\e[0m"
-    df -i ${MOUNT_PATH}
+    if [ ${VERBOSE} = true ]; then
+        echo -e "\e[32m\tCreating\e[36m ${NUM_FILES} \e[32mfiles in\e[36m ${MOUNT_PATH}\e[0m"
+        for (( i=1; i <= ${NUM_FILES}; i++ )); do
+            touch ${MOUNT_PATH}/$i.file
+            if [[ $((i % 1000)) = 0 ]]; then
+                progress=$(echo "scale=3; ${i}/${NUM_FILES}*100" | bc)
+                echo -ne "\e[?25l\e[s\e[2K${progress}% complete (files $i of ${NUM_FILES})\e[u"
+                # echo -ne "\e[?25l\e[s\e[2K$i of ${NUM_FILES}\e[u"
+            fi
+        done
+        echo -e "\n\e[36m\t${NUM_FILES} \e[32mfiles have been created\e[0m"
+        df -i ${MOUNT_PATH}
+    else
+        for (( i=1; i <= ${NUM_FILES}; i++ )); do
+            touch ${MOUNT_PATH}/$i.file
+        done
+    fi
     sh -c 'sync && echo 2 > /proc/sys/vm/drop_caches' 
 }
 
 function FN_MOUNT_DISK {
     if (cat /proc/mounts | grep ${DISK_PATH}); then
-        echo -e "\e[31m\tDevice \e[36m ${DISK_PATH} \e[31m already mounted\e[0m"
-        echo -e "\e[32m\tUn mounting a\e[36m ${DISK_PATH} \e[0m"
+        if [ ${VERBOSE} = true ]; then
+            echo -e "\e[31m\tDevice \e[36m ${DISK_PATH} \e[31m already mounted\e[0m"
+            echo -e "\e[32m\tUn mounting a\e[36m ${DISK_PATH} \e[0m"
+        fi
         umount ${DISK_PATH}
     fi
-    echo -e "\e[32m\tMounting disk\e[36m ${DISK_PATH} \e[32mto\e[36m ${MOUNT_PATH}\e[0m"
+    if [ ${VERBOSE} = true ]; then
+        echo -e "\e[32m\tMounting disk\e[36m ${DISK_PATH} \e[32mto\e[36m ${MOUNT_PATH}\e[0m"
+    fi
     mount ${DISK_PATH} ${MOUNT_PATH}
-    df -i ${MOUNT_PATH}
+    if [ ${VERBOSE} = true ]; then
+        df -i ${MOUNT_PATH}
+    fi
 }
 
 function FN_CREATE_FS_EXT4 {
-    echo -e "\e[32m\tCreating filesystem\e[36m ext4 \e[32mwith\e[36m ${INODES_NUM} \e[32minodes\e[0m"
-    mkfs -t ext4 -N ${INODES_NUM} ${DISK_PATH}
-    echo -e "\e[32m\tFilesystem has been created\e[0m"
+    if [ ${VERBOSE} = true ]; then
+        echo -e "\e[32m\tCreating filesystem\e[36m ext4 \e[32mwith\e[36m ${INODES_NUM} \e[32minodes\e[0m"
+        mkfs -t ext4 -N ${INODES_NUM} ${DISK_PATH}
+        echo -e "\e[32m\tFilesystem has been created\e[0m"
+    else
+        mkfs -t ext4 -N ${INODES_NUM} ${DISK_PATH} > /dev/null 2>&1
+    fi
 }
 
 function FN_WIPE_DISK {
-    echo -e "\e[32m\tWipe\e[36m ${DISK_PATH} \e[32mwith zeros... \e[0m"
-    dd if=/dev/zero of=${DISK_PATH} bs=$((1024*1024)) count=$((${DISK_SIZE}/$((1024*1024)))) status=progress
-    echo -e "\e[32m\tWiping complete\e[0m"
+    if [ ${VERBOSE} = true ]; then
+        echo -e "\e[32m\tWipe\e[36m ${DISK_PATH} \e[32mwith zeros... \e[0m"
+        dd if=/dev/zero of=${DISK_PATH} bs=$((1024*1024)) count=$((${DISK_SIZE}/$((1024*1024)))) status=progress
+        echo -e "\e[32m\tWiping complete\e[0m"
+    else
+        dd if=/dev/zero of=${DISK_PATH} bs=$((1024*1024)) count=$((${DISK_SIZE}/$((1024*1024)))) > /dev/null 2>&1
+    fi
 }
 
 function FN_MKDIR {
     if [ -d ${MOUNT_PATH}/ ]; then
         if (cat /proc/mounts | grep ${DISK_PATH}); then
-            echo -e "\e[31m\tDevice \e[36m ${DISK_PATH} \e[31m already mounted\e[0m"
-            echo -e "\e[32m\tUn mounting a\e[36m ${DISK_PATH} \e[0m"
+            if [ ${VERBOSE} = true ]; then
+                echo -e "\e[31m\tDevice \e[36m ${DISK_PATH} \e[31m already mounted\e[0m"
+                echo -e "\e[32m\tUn mounting a\e[36m ${DISK_PATH} \e[0m"
+            fi
             umount ${DISK_PATH}
         fi
-        echo -e "\e[32m\tDeleting directory\e[36m $MOUNT_PATH \e[0m"
+        if [ ${VERBOSE} = true ]; then
+            echo -e "\e[32m\tDeleting directory\e[36m $MOUNT_PATH \e[0m"
+        fi
         rm -rf $MOUNT_PATH/
     fi
-
-    echo -e "\e[32m\tCreating directory\e[36m ${MOUNT_PATH}\e[0m"
+    if [ ${VERBOSE} = true ]; then
+        echo -e "\e[32m\tCreating directory\e[36m ${MOUNT_PATH}\e[0m"
+    fi
     mkdir -p ${MOUNT_PATH}
-    ls -la /test/ | head -n 5
+    if [ ${VERBOSE} = true ]; then
+        ls -la /test/ | head -n 5
+    fi
 }
 
 function FN_PREPS_FN {
     # Prepare directory
     FN_MKDIR
-    FN_DECORATE
+    if [ ${VERBOSE} = true ]; then FN_DECORATE; fi
 
     # Wipe disk
     FN_WIPE_DISK
-    FN_DECORATE
+    if [ ${VERBOSE} = true ]; then FN_DECORATE; fi
 
     # Prepare filesystem for test
     FN_CREATE_FS_EXT4
-    FN_DECORATE
+    if [ ${VERBOSE} = true ]; then FN_DECORATE; fi
 
     # Mount disk
     FN_MOUNT_DISK
-    FN_DECORATE
+    if [ ${VERBOSE} = true ]; then FN_DECORATE; fi
 
     # Create many files
-    time FN_CREATE_FILES
-    FN_DECORATE
+    FN_CREATE_FILES
+    if [ ${VERBOSE} = true ]; then FN_DECORATE; fi
 }
 
 echo -e "\n\n\n\tVariable check:"
 FN_DECORATE
 FN_VARS_CHECK | column -t
 FN_VARS_CHECK &> /dev/null
-FN_DECORATE
 
 # If FN_VARS_CHECK generate $EXITCODE = 1, exit 1
 if [ $EXITCODE = 1 ]; then
@@ -135,69 +162,34 @@ if [ $EXITCODE = 1 ]; then
 fi
 
 # ----------------------------------------
+# Run all prepare function
 FN_PREPS_FN
 
 # ----------------------------------------
-# Test 1
-# Get list of files through standart ls command without screen output
-sh -c 'sync && echo 2 > /proc/sys/vm/drop_caches' 
-echo -e "\e[33m\tExecution time for command:\e[36m ls ${MOUNT_PATH}\e[0m"
-time ls ${MOUNT_PATH} | tail -n 0
-FN_DECORATE
+# Run tests
+ArrayOfTest=("time ls ${MOUNT_PATH} | tail -n 0" "time ls -f ${MOUNT_PATH} | tail -n 0" "time rm -rf ${MOUNT_PATH}/" "time rm -rf ${MOUNT_PATH}/*" "time find ${MOUNT_PATH}/ -type f -exec rm -v {} \;" "time find ${MOUNT_PATH}/ -type f -delete" "time cd ${MOUNT_PATH}/ ; ls -f . | xargs -n 100 rm")
 
-# Tests 2
-# Get list of files through ls command without sort without screen output
-sh -c 'sync && echo 2 > /proc/sys/vm/drop_caches' 
-echo -e "\e[33m    Execution time for command:\e[36m ls -f ${MOUNT_PATH} \e[0m"
-time ls -f ${MOUNT_PATH} | tail -n 0
-FN_DECORATE
+for array in ${!ArrayOfTest[@]}; do
+    if [ ${VERBOSE} = true ]; then
+        FN_DECORATE
+        FN_PREPS_FN
+    else
+        FN_PREPS_FN > /dev/null 2>&1
+    fi
+    sh -c 'sync && echo 2 > /proc/sys/vm/drop_caches'
+    echo -e "\e[32m\n\tTest = ${array} of ${#ArrayOfTest[*]}\e[0m"
+    echo -e "\e[33m\tExecution time for command:\e[36m ${ArrayOfTest[${array}]} \e[0m"
+    
+    if [ ${VERBOSE} = true ]; then
+        time "${ArrayOfTest[${array}]}"
+        ls -f ${MOUNT_PATH}
+        df -i ${MOUNT_PATH}
+    else
+        time "#${ArrayOfTest[${array}]}" > /dev/null 2>&1
+    fi
+done
 
-# Test 3
-# Remove all files rm -r
-FN_PREPS_FN
-sh -c 'sync && echo 2 > /proc/sys/vm/drop_caches' 
-echo -e "\e[33m\tExecution time for command:\e[36m rm -r ${MOUNT_PATH}\e[0m"
-time rm -rf ${MOUNT_PATH}/*
-ls -f ${MOUNT_PATH}
-df -i ${MOUNT_PATH}
 FN_DECORATE
-
-# ----------------------------------------
-# Test 4
-# Remove all files rm ./*
-FN_PREPS_FN
-sh -c 'sync && echo 2 > /proc/sys/vm/drop_caches' 
-echo -e "\e[33m\tExecution time for command:\e[36m rm -r ${MOUNT_PATH} \e[0m"
-time rm -rf ${MOUNT_PATH}/*
-ls -f ${MOUNT_PATH}
-df -i ${MOUNT_PATH}
-FN_DECORATE
-
-exit 0
-
-# Test 5
-# Remove all files rm ./*
-FN_PREPS_FN
-sh -c 'sync && echo 2 > /proc/sys/vm/drop_caches' 
-time find ${MOUNT_PATH}/ -type f -exec rm -v {} \;
-ls -f ${MOUNT_PATH}
-df -i ${MOUNT_PATH}
-FN_DECORATE
-
-# Test 6
-FN_PREPS_FN
-sh -c 'sync && echo 2 > /proc/sys/vm/drop_caches' 
-time find ${MOUNT_PATH}/ -type f -delete
-ls -f ${MOUNT_PATH}
-df -i ${MOUNT_PATH}
-FN_DECORATE
-
-# Test 7
-FN_PREPS_FN
-sh -c 'sync && echo 2 > /proc/sys/vm/drop_caches' 
-time cd ${MOUNT_PATH}/ ; ls -f . | xargs -n 100 rm
-ls -f ${MOUNT_PATH}
-df -i ${MOUNT_PATH}
-FN_DECORATE
+echo -e "\e[31m\n\tAll tests complete\e[0m"
 
 exit 0
